7444f79f18b3bdf466bb0f8622b2fbf9ee176d12
Adam Silver <adambsilver@gmail.com>
2022-12-05 14:37:20 +0000

    Upgrade documentation task

--- app/routes/report.js
+++ app/routes/report.js
@@ -3,17 +3,47 @@ const _ = require('lodash')
 module.exports = router => {
 
   router.all(['/report', '/report/*'], (req, res, next) => {
-    // res.locals.signedIn = true
-    // res.locals.reportService = true
-
     res.locals.isPublic = _.get(req.session.data, 'report[type-of-report]') == 'public'
     res.locals.isEmployer = !res.locals.isPublic
-
     next()
   })
 
   router.post('/report', (req, res) => {
     res.redirect('/report/submit/review')
+  })
+
+  router.get('/report/submit/review', (req, res) => {
+    const files = _.get(req.session.data, 'report.documentation.uploaded-files') || {}
+    const fileRows = []
+
+    for (const [fileId, file] of Object.entries(files)) {
+      let fileTypes = file.type ? file.type.join(', ') : 'No type'
+      fileTypes = 'Type:<br />' + fileTypes.replace('Other', `Other: ${file['other-type']}`)
+
+      fileRows.push({
+        key: {
+          html: `<a href="#">${file.filename}</a>`
+        },
+        value: {
+          html: fileTypes
+        },
+        actions: {
+          items: [
+            {
+              text: 'Change',
+              href: `/report/documentation/edit-file/${fileId}`
+            },
+            {
+              text: 'Delete',
+              href: '#'
+            }
+          ]
+        }
+      })
+    }
+    res.render('report/submit/review', {
+      fileRows
+    })
   })
 
   router.post('/report/submit/review', (req, res) => {
