7f03e48c2932a771c988cee17958929c0d840c87
Adam Silver <adambsilver@gmail.com>
2022-12-12 14:47:07 +0000

    Update previous allegations and previous misconduct flow

--- app/routes/report--previous-misconduct.js
+++ app/routes/report--previous-misconduct.js
@@ -3,14 +3,14 @@ const _ = require('lodash')
 module.exports = router => {
 
   router.post('/report/previous-misconduct/any-previous', (req, res) => {
-    res.redirect('/report/previous-misconduct/previous-misconduct')
+    if(req.session.data.report.previousMisconduct.anyPrevious == 'Yes') {
+      res.redirect('/report/previous-misconduct/previous-misconduct')
+    } else {
+      res.redirect('/report/previous-misconduct/check-answers')
+    }
   })
 
   router.post('/report/previous-misconduct/previous-misconduct', (req, res) => {
-    res.redirect('/report/previous-misconduct/previous-misconduct-summary')
-  })
-
-  router.post('/report/previous-misconduct/previous-misconduct-summary', (req, res) => {
     res.redirect('/report/previous-misconduct/check-answers')
   })
 
