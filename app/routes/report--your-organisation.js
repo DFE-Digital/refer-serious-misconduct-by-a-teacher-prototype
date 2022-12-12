6f8ea516be88b49748e3540c5b4867d34b82641b
Adam Silver <adambsilver@gmail.com>
2022-12-12 13:33:09 +0000

    Update your details and your org task

--- app/routes/report--your-organisation.js
+++ app/routes/report--your-organisation.js
@@ -2,11 +2,7 @@ const _ = require('lodash')
 
 module.exports = router => {
 
-  router.post('/report/your-organisation/name', (req, res) => {
-    res.redirect('/report/your-organisation/address')
-  })
-
-  router.post('/report/your-organisation/address', (req, res) => {
+  router.post('/report/your-organisation/organisation', (req, res) => {
     res.redirect('/report/your-organisation/check-answers')
   })
 
