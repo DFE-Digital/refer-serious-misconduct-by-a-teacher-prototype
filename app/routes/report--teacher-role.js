47802913078fde83683ebe17483f70b9239f7376
Adam Silver <adambsilver@gmail.com>
2022-12-12 10:38:12 +0000

    Update about their role task for the employer journey

--- app/routes/report--teacher-role.js
+++ app/routes/report--teacher-role.js
@@ -3,15 +3,31 @@ const _ = require('lodash')
 module.exports = router => {
 
   router.post('/report/teacher-role/start-date', (req, res) => {
-    res.redirect('/report/teacher-role/end-date')
-  })
-
-  router.post('/report/teacher-role/end-date', (req, res) => {
     res.redirect('/report/teacher-role/job-title')
   })
 
   router.post('/report/teacher-role/job-title', (req, res) => {
-    res.redirect('/report/teacher-role/school')
+    res.redirect('/report/teacher-role/same-organisation')
+  })
+
+  router.post('/report/teacher-role/same-organisation', (req, res) => {
+    if(req.session.data.report['teacher-role']['same-organisation'] === 'Yes') {
+      res.redirect('/report/teacher-role/duties')
+    } else {
+      res.redirect('/report/teacher-role/know-where-they-worked')
+    }
+  })
+
+  router.post('/report/teacher-role/know-where-they-worked', (req, res) => {
+    if(req.session.data.report['teacher-role']['know-where-they-worked'] === 'Yes') {
+      res.redirect('/report/teacher-role/where-they-worked')
+    } else {
+      res.redirect('/report/teacher-role/duties')
+    }
+  })
+
+  router.post('/report/teacher-role/where-they-worked', (req, res) => {
+    res.redirect('/report/teacher-role/duties')
   })
 
   router.post('/report/teacher-role/school', (req, res) => {
@@ -19,22 +35,42 @@ module.exports = router => {
   })
 
   router.post('/report/teacher-role/duties', (req, res) => {
-    if(res.locals.isPublic) {
-      res.redirect('/report/teacher-role/check-answers')
+    res.redirect('/report/teacher-role/still-employed')
+  })
+
+  router.post('/report/teacher-role/still-employed', (req, res) => {
+    if(req.session.data.report['teacher-role']['still-employed'] === 'No') {
+      res.redirect('/report/teacher-role/when-they-left')
     } else {
-      res.redirect('/report/teacher-role/teaching-somewhere-else')
+      res.redirect('/report/teacher-role/check-answers')
     }
+  })
+
+  router.post('/report/teacher-role/when-they-left', (req, res) => {
+    res.redirect('/report/teacher-role/why-they-left')
+  })
+
+  router.post('/report/teacher-role/why-they-left', (req, res) => {
+    res.redirect('/report/teacher-role/teaching-somewhere-else')
   })
 
   router.post('/report/teacher-role/teaching-somewhere-else', (req, res) => {
     if(req.session.data.report['teacher-role']['teaching-elsewhere'] == 'Yes') {
-      res.redirect('/report/teacher-role/teaching-where-now')
+      res.redirect('/report/teacher-role/know-where-they-work')
     } else {
       res.redirect('/report/teacher-role/check-answers')
     }
   })
 
-  router.post('/report/teacher-role/teaching-where-now', (req, res) => {
+  router.post('/report/teacher-role/know-where-they-work', (req, res) => {
+    if(req.session.data.report['teacher-role']['know-where-they-work'] === 'Yes') {
+      res.redirect('/report/teacher-role/where-they-work')
+    } else {
+      res.redirect('/report/teacher-role/check-answers')
+    }
+  })
+
+  router.post('/report/teacher-role/where-they-work', (req, res) => {
     res.redirect('/report/teacher-role/check-answers')
   })
 
