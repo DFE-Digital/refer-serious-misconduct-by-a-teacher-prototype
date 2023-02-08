68bf5813ff0ad5370b7d7b6cda870893722ac67c
Adam Silver <adambsilver@gmail.com>
2023-02-08 12:55:50 +0000

    Incomplete sections

--- app/routes/report--your-details.js
+++ app/routes/report--your-details.js
@@ -1,5 +1,15 @@
 const _ = require('lodash')
 
+function getFirstIncompleteSection(data) {
+  let firstName = _.get(data, 'report.your-details.firstName')
+  let lastName = _.get(data, 'report.your-details.lastName')
+  if(!firstName or !lastName) {
+    return {
+      name: 'name'
+    }
+  }
+}
+
 module.exports = router => {
 
   router.post('/report/your-details/name', (req, res) => {
@@ -16,6 +26,11 @@ module.exports = router => {
 
   router.post('/report/your-details/telephone', (req, res) => {
     res.redirect('/report/your-details/check-answers')
+  })
+
+  router.get('/report/your-details/check-answers', (req, res) => {
+    getFirstIncompleteSection(req.session.data)
+    res.render('report/your-details/check-answers')
   })
 
   router.post('/report/your-details/check-answers', (req, res) => {
