0813c65d54ad0d0f8872f972d5be04c111dbc580
Adam Silver <adambsilver@gmail.com>
2023-02-09 09:30:21 +0000

    Added all conditions

--- app/routes/report--your-details.js
+++ app/routes/report--your-details.js
@@ -1,12 +1,20 @@
 const _ = require('lodash')
 
 function getFirstIncompleteSection(data) {
+  let type = _.get(data, 'report.type-of-report')
   let firstName = _.get(data, 'report.your-details.firstName')
   let lastName = _.get(data, 'report.your-details.lastName')
-  if(!firstName or !lastName) {
-    return {
-      name: 'name'
-    }
+  let jobTitle = _.get(data, 'report.your-details.job-title')
+  let telephone = _.get(data, 'report.your-details.telephone')
+  if(!firstName || !lastName) {
+    return 'name'
+  }
+  console.log(data)
+  if(type == 'employer' && !jobTitle) {
+    return 'job-title'
+  }
+  if(!telephone) {
+    return 'telephone'
   }
 }
 
@@ -29,8 +37,9 @@ module.exports = router => {
   })
 
   router.get('/report/your-details/check-answers', (req, res) => {
-    getFirstIncompleteSection(req.session.data)
-    res.render('report/your-details/check-answers')
+    res.render('report/your-details/check-answers', {
+      incompleteSection: getFirstIncompleteSection(req.session.data)
+    })
   })
 
   router.post('/report/your-details/check-answers', (req, res) => {
